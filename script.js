79cfc64aabf3489df3ef4915cf05d80a7582f6d2
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-01-27 20:55:57 +0100

    Create a responsive sidebar

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-const currentYear = document.querySelector("span.year");
-const hamburger = document.querySelector("div.hamburger-menu > img");
+let hamburger;
+let currentYear;
+let navbarLinks;
+let mainContent;
 
 const date = new Date();
 const fullYear = date.getFullYear();
 
-currentYear.textContent = fullYear;
+const main = () => {
+  prepareDOMElements();
+  prepareDOMEvents();
+  setCurrentYear();
+}
+
+const prepareDOMElements = () => {
+  hamburger = document.querySelector("div.hamburger-menu > img");
+  currentYear = document.querySelector("span.year");
+  navbarLinks = document.querySelector("div.navbar-links > ul");
+  mainContent = document.querySelector("main");
+}
+
+const prepareDOMEvents = () => {
+  hamburger.addEventListener("click", toggleSidebar);
+  mainContent.addEventListener("click", closeSidebar);
+}
+
+const setCurrentYear = () => {
+  currentYear.textContent = fullYear;  
+}
+
+const toggleSidebar = () => {
+  navbarLinks.classList.toggle("sidebar-collapse");
+}
+
+const closeSidebar = () => {
+  if (navbarLinks.classList.contains("sidebar-collapse")) {
+    navbarLinks.classList.remove("sidebar-collapse");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", main);
