7515eef67405474f7a6cce88c6491d3954cc7557
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-05-28 20:35:07 +0200

    Modify functionality for modal showup

--- js/script.js
+++ js/script.js
@@ -2,7 +2,21 @@ let currentYear;
 let hourSpan;
 let dateSpan;
 let addNoteButton;
+let saveButton;
+let cancelButton;
 let deleteAllButton;
+let deleteNoteButton;
+let notesContainer;
+let notesPanel;
+let notesShadow;
+let notesSelect;
+let notesTextarea;
+let error;
+
+let selectedValue;
+
+// Każda notatka będzie miała własny, unikalny identyfikator
+let noteId = 0;
 
 const main = () => { 
   prepareDOMElements();
@@ -16,11 +30,22 @@ const prepareDOMElements = () => {
   hourSpan = document.querySelector(".menu__date > span");
   dateSpan = document.querySelector(".menu__time > span");
   addNoteButton = document.querySelector(".menu__button--add");
+  saveButton = document.querySelector(".notes__panel-button--save");
+  cancelButton = document.querySelector(".notes__panel-button--cancel");
   deleteAllButton = document.querySelector(".menu__button--delete");
+  deleteNoteButton = document.querySelector(".notes__delete-btn");
+  notesContainer = document.querySelector(".notes__container");
+  notesPanel = document.querySelector(".notes__panel");
+  notesShadow = document.querySelector(".notes__shadow");
+  notesSelect = document.querySelector(".notes__panel-select");
+  notesTextarea = document.querySelector(".notes__panel-text");
+  error = document.querySelector(".notes__panel-error");
 }
 
 const prepareDOMEvents = () => {
-  addNoteButton.addEventListener("click", addNote);
+  addNoteButton.addEventListener("click", openPanel);
+  cancelButton.addEventListener("click", closePanel);
+  window.addEventListener("click", (event) => event.target === notesShadow ? closePanel() : false);
 }
 
 const setDateAndTime = () => {
@@ -38,8 +63,17 @@ const setDateAndTime = () => {
 
 const updateDateAndTime = () => setInterval(setDateAndTime, 1000);
 
-const addNote = () => {
-  console.log("addNote");
+const openPanel = () => {
+  notesPanel.classList.add("active", "animation-in");
+  notesShadow.classList.add("active", "animation-in");
+}
+
+const closePanel = () => {
+  notesPanel.classList.remove("active","animation-in");
+  notesShadow.classList.remove("active", "animation-in");
+  notesSelect.selectedIndex = 0;
+  notesTextarea.value = "";
+  error.style.display = "none";
 }
 
 document.addEventListener("DOMContentLoaded", main);
